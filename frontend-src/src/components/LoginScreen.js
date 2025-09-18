ea1828126e30a1b3cb795c9a9c4f9324e4acc8a1
Rishi Satsangi <rishi.satsangi@gmail.com>
2025-09-18 01:23:18 +0000

    change some branding

--- frontend-src/src/components/LoginScreen.js
+++ frontend-src/src/components/LoginScreen.js
@@ -12,7 +12,7 @@ function LoginScreen() {
             />
             <Container shadow="xl" m="xs" p="xs" className={classes.container} size="lg">
                 <Box ta='center'>
-                    <Image src='/img/logo.001.png' w={{ base: 300, xs: 480, sm: 640 }} height='auto' display='inline-block'  className='AppLogo' my='xs' />
+                    <Image src='/img/logo.001.png' w={{ base: 300, xs: 300, sm: 400 }} height='auto' display='inline-block'  className='AppLogo' my='xs' />
                     <Text pt='lg' fw={700} size='xl'>Add  / remove the currently playing track from multiple playlists at once.</Text>
                     <Text pt='md' pb='md' fw={300} >A new way of interacting with Spotify.</Text>
                     <Button
@@ -31,11 +31,11 @@ function LoginScreen() {
                 <Container p={{ base: 'xs', sm: 'xl' }} >
                     <Text mt='lg' fw={400} size='lg' mx={{ base: 'sm', xs: 'md', sm: 'xl'}} >Struggling with playlists that don't quite hit the mark? We've all been there—adding songs that seem right at the time, only to find yourself constantly hitting skip. Maybe that track would be perfect in a different playlist, but who has time to sift through the Spotify UI?</Text>
                     <Box ta='center'>
-                        <Image display='inline-block' my='xl' style={{ borderRadius: '50px' }} src='https://cdn.midjourney.com/f300ebbe-1024-4a7c-b9ca-4fb84a708df4/0_1.png' h={{ base: 135, xs: 160, sm: 225 }} w='auto' />
+                        <Image display='inline-block' my='xl' style={{ borderRadius: '50px' }} src='https://cdn.midjourney.com/f300ebbe-1024-4a7c-b9ca-4fb84a708df4/0_1.png' h={{ base: 160, xs: 240, sm: 320 }} w='auto' />
                     </Box>
                     <Text mt='lg' fw={400} size='lg' mx={{ base: 'sm', xs: 'md', sm: 'xl'}}>Enter Playlister: the app that simplifies playlist management. With just a few clicks, you can effortlessly add or remove the currently playing track from multiple playlists at once. Get the right songs in the right places—no more skips, just smooth listening.</Text>
                     <Box ta='center'>
-                        <Image display='inline-block' my='xl'  style={{ borderRadius: '50px' }} src='https://cdn.midjourney.com/fec2f169-47a5-49cf-a757-3d78d5cc49cf/0_0.png'  h={{ base: 135, xs: 160, sm: 225 }} w='auto' />
+                        <Image display='inline-block' my='xl'  style={{ borderRadius: '50px' }} src='https://cdn.midjourney.com/fec2f169-47a5-49cf-a757-3d78d5cc49cf/0_0.png'  h={{ base: 160, xs: 240, sm: 320 }} w='auto' />
                     </Box>
                 </Container>
             </Container>
