be7ce344dbc72cdc16fa5c000a7812d4f665d154
Rishi Satsangi <rishi.satsangi@gmail.com>
2025-10-27 03:49:57 +0000

    update build.sh to build:deploy in same command

--- build.sh
+++ build.sh
@@ -3,14 +3,8 @@
 # build frontend
 cd frontend-src
 npm install
-echo VITE_ENVIRONMENT="production" > .env
-npm run build
+VITE_ENVIRONMENT="production" && npm run build:deploy
 echo VITE_ENVIRONMENT="development" > .env
-
-# copy built assets to `/src/public`
-cd ../
-cd deploy-tools
-node deploy.js
 
 # build the express server (backend)
 cd ../
